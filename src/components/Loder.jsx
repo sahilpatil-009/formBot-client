f3641c9ff6dbaf10c71ca2949b4574ef1c1ebf5b
Spatil <sahilpatil67049@gmail.com>
2025-01-01 11:49:44 +0530

    Add Loading component

---
+++ src/components/Loder.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Bars, Oval } from "react-loader-spinner";
+
+const Loder = () => {
+  return (
+    <Bars
+  height="80"
+  width="80"
+  color="#1a5fff"
+  ariaLabel="bars-loading"
+  wrapperStyle={{}}
+  wrapperClass=""
+  visible={true}
+  />
+  );
+};
+
+export default Loder;
